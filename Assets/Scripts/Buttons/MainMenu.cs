aec2cf2c998d4c6ad1e83f9a492acc1bf4febe47
Ansver <Sashachelmodeev@gmail.com>
2023-09-30 10:09:55 +0300

    Забилдил проект 3 уровня. МЕНЮ + ХАБ + МЕНЮ. Сделал меню.

---
+++ Assets/Scripts/Buttons/MainMenu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject creatorsPanel; // Ссылка на панель с информацией о создателях.
+    public GameObject controlsPanel; // Ссылка на панель с управлением.
+
+    private bool creatorsPanelActive = false; // Флаг, показывающий, активна ли панель создателей.
+    private bool controlsPanelActive = false; // Флаг, показывающий, активна ли панель управления.
+
+    private void Start()
+    {
+        // Изначально скрываем панели с информацией о создателях и управлением.
+        creatorsPanel.SetActive(false);
+        controlsPanel.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        // Загружаем первый уровень (или другой уровень, который вы хотите использовать для начала игры).
+        SceneManager.LoadScene("Level1"); // Замените "Level1" на имя вашего уровня.
+    }
+
+    public void ShowCreators()
+    {
+        // Переключаем состояние панели с информацией о создателях.
+        creatorsPanelActive = !creatorsPanelActive;
+        creatorsPanel.SetActive(creatorsPanelActive);
+    }
+
+    public void ShowControls()
+    {
+        // Переключаем состояние панели с управлением.
+        controlsPanelActive = !controlsPanelActive;
+        controlsPanel.SetActive(controlsPanelActive);
+    }
+
+    public void QuitGame()
+    {
+        // Выход из игры (работает в сборке приложения).
+        Application.Quit();
+    }
+}
